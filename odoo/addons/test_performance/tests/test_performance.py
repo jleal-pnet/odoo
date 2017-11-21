2dff071f38e7767e14f652dee66dad5050bf774c
Thibault Delavallée <tde@odoo.com>
2017-11-21 18:42:53 +0100

    [SPLIT] test_performance: split between base and mail performance tests
    Test performance will now hold the base class for performance tests as
    well as tests related to the ORM, depending only on base. A new module
    test_mail is introduced at this commit that contains performance tests
    related to mail module. This commit contains only code move and should
    not impact anything.
    
    Future commits will move mail tests into test_mail so that all mail
    related tests are located in the same optional module. This allows
    notably to avoid creating a lot of unnecessary tables when installing
    mail module on production databases.

--- odoo/addons/test_performance/tests/test_performance.py
+++ odoo/addons/test_performance/tests/test_performance.py
@@ -86,25 +86,6 @@ class TestPerformance(TransactionCase):
         for record in records:
             record.value_pc
 
-    @queryCount(admin=3, demo=3)
-    def test_read_mail(self):
-        """ Read records inheriting from 'mail.thread'. """
-        records = self.env['test_performance.mail'].search([])
-        self.assertEqual(len(records), 5)
-        self.resetQueryCount()
-
-        # without cache
-        for record in records:
-            record.partner_id.country_id.name
-
-        # with cache
-        for record in records:
-            record.partner_id.country_id.name
-
-        # value_pc must have been prefetched, too
-        for record in records:
-            record.value_pc
-
     @queryCount(admin=1, demo=1)
     def test_write_base(self):
         """ Write records (no recomputation). """
@@ -122,34 +103,6 @@ class TestPerformance(TransactionCase):
         self.resetQueryCount()
 
         records.write({'value': self.int(20)})
-
-    @queryCount(admin=4, demo=4)
-    def test_write_mail(self):
-        """ Write records inheriting from 'mail.thread' (no recomputation). """
-        records = self.env['test_performance.mail'].search([])
-        self.assertEqual(len(records), 5)
-        self.resetQueryCount()
-
-        records.write({'name': self.str('X')})
-
-    @queryCount(admin=6, demo=6)
-    def test_write_mail_with_recomputation(self):
-        """ Write records inheriting from 'mail.thread' (with recomputation). """
-        records = self.env['test_performance.mail'].search([])
-        self.assertEqual(len(records), 5)
-        self.resetQueryCount()
-
-        records.write({'value': self.int(20)})
-
-    @queryCount(admin=33, demo=47)
-    def test_write_mail_with_tracking(self):
-        """ Write records inheriting from 'mail.thread' (with field tracking). """
-        record = self.env['test_performance.mail'].search([], limit=1)
-        self.assertEqual(len(record), 1)
-        self.resetQueryCount()
-
-        record.track = self.str('X')
-
     @queryCount(admin=6, demo=6)
     def test_create_base(self):
         """ Create records. """
