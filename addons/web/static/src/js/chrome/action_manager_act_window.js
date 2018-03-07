0fdc86649734e21b20b20844bba61d74d8f37841
Tejas Shahu <tsh@odoo.com>
2018-03-07 17:02:39 +0530

    [IMP] framework: reload page after clicking on cancel button of wizard. Before this commit: Page was not reloaded after clicking on cancel button of wizard.
    After this commit:
    Page will be reloaded after clicking on cancel button of wizard.
    Because in one of the logistic imp task the manufactured quantity and consumed quantity were not updating on page.
    So manual reload was required.

--- addons/web/static/src/js/chrome/action_manager_act_window.js
+++ addons/web/static/src/js/chrome/action_manager_act_window.js
@@ -714,7 +714,9 @@ ActionManager.include({
         var def = $.Deferred();
 
         // determine the action to execute according to the actionData
-        if (actionData.special) {
+        if (actionData.special && actionData.options.reload) {
+            def = $.when({type: 'ir.actions.client', tag: 'reload'});
+        } else if (actionData.special) {
             def = $.when({type: 'ir.actions.act_window_close', infos: 'special'});
         } else if (actionData.type === 'object') {
             // call a Python Object method, which may return an action to execute
