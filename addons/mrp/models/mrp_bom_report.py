75bdc52ac8ee0f68232d752782aa40112ef98620
Arnold Moyaux <arm@odoo.com>
2018-03-15 13:02:37 +0100

    [IMP] mrp: move operations inside the report

--- addons/mrp/models/mrp_bom_report.py
+++ addons/mrp/models/mrp_bom_report.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import json
 from odoo import api, models
 from odoo.tools import config, float_round
 
@@ -10,48 +9,29 @@ class MrpBomReport(models.TransientModel):
     _name = 'mrp.bom.report'
     _description = "Mrp Bom Report"
 
-    def _get_operations(self, bom, product, parent, qty, bom_ids):
-        operations = []
-        if parent:
-            qty = parent.product_uom_id._compute_quantity(qty, bom.product_uom_id, round=False)
-        for line in bom.bom_line_ids:
-            if line._skip_bom_line(product):
-                continue
-            line_quantity = line.product_qty * qty
-            if parent:
-                line_quantity = line.product_qty * qty
-            if line.bom_id.routing_id and line.bom_id.id not in bom_ids:
-                bom_ids.append(line.bom_id.id)
-                operations = self._get_operation_line(line.bom_id.routing_id, line.bom_id, parent, (qty / bom.product_qty))
-            if line.child_bom_id:
-                qty = line.product_uom_id._compute_quantity(line_quantity, line.child_bom_id.product_uom_id, round=False)
-                _operations = self._get_operations(line.child_bom_id, line.product_id, line, qty, bom_ids)
-                operations += _operations
-        return operations
-
-    def _get_operation_line(self, routing, bom, parent, qty):
+    def _get_operation_line(self, routing, qty, level):
         operations = []
         total = 0.0
         for operation in routing.operation_ids:
-            rounding = bom.product_uom_id.rounding
-            cycle_number = float_round((1.0 / operation.workcenter_id.capacity), precision_rounding=rounding)
-            if not parent:
-                cycle_number = cycle_number * qty
+            cycle_number = float_round(qty / operation.workcenter_id.capacity, precision_digits=1, rounding_method='UP')
             duration_expected = cycle_number * operation.time_cycle * 100.0 / operation.workcenter_id.time_efficiency
-            if parent:
-                duration_expected = duration_expected * qty
             duration_expected += operation.workcenter_id.time_stop + operation.workcenter_id.time_start
             total = ((duration_expected / 60.0) * operation.workcenter_id.costs_hour)
             operations.append({
+                'level': level or 0,
                 'operation': operation,
-                'name': operation.name + ' - ' + bom.product_tmpl_id.name,
+                'name': operation.name + ' - ' + operation.workcenter_id.name,
                 'duration_expected': duration_expected,
-                'total': float_round(total, precision_rounding=rounding),
+                'total': float_round(total, precision_rounding=self.env.user.company_id.currency_id.rounding),
             })
         return operations
 
     def _get_price(self, bom, factor):
         price = 0
+        if bom.routing_id:
+            operations = self._get_operation_line(bom.routing_id, factor, 0)
+            price += sum([op['total'] for op in operations])
+
         for line in bom.bom_line_ids:
             if line.child_bom_id:
                 qty = line.product_uom_id._compute_quantity(line.product_qty * factor, line.child_bom_id.product_uom_id)
@@ -63,15 +43,13 @@ class MrpBomReport(models.TransientModel):
         return price
 
     @api.model
-    def get_lines(self, bom_id=False, line_qty=False, line_id=False, level=False, searchQty=0, searchVariant=False, report_type='html'):
+    def get_report_data(self, bom_id, searchQty=0, searchVariant=False):
         lines = {}
         bom = self.env['mrp.bom'].browse(bom_id)
         bom_quantity = float(searchQty) or bom.product_qty
         bom_product_variants = {}
         bom_uom_name = ''
-        if line_id:
-            current_line = self.env['mrp.bom.line'].browse(int(line_id))
-            bom_quantity = current_line.product_uom_id._compute_quantity(line_qty, bom.product_uom_id)
+
         if bom:
             bom_uom_name = bom.product_uom_id.name
 
@@ -80,50 +58,8 @@ class MrpBomReport(models.TransientModel):
                 for variant in bom.product_tmpl_id.product_variant_ids:
                     bom_product_variants[variant.id] = variant.display_name
 
-            # Display bom components for current selected product variant
-            if searchVariant:
-                product = self.env['product.product'].browse(int(searchVariant))
-            else:
-                product = bom.product_id or bom.product_tmpl_id.product_variant_id
+        lines = self.get_bom_line(bom_id, product_id=searchVariant, line_qty=bom_quantity)
 
-            components = []
-            operations_data = self._get_operations(bom, product, False, bom_quantity, [])
-            lines = {
-                'report_type': report_type,
-                'bom': bom,
-                'bom_qty': bom_quantity,
-                'bom_prod_name': product.display_name,
-                'currency': self.env.user.company_id.currency_id,
-                'product': product,
-                'total': 0.0,
-                'operations': operations_data,
-                'operations_total': sum([op['total'] for op in operations_data])
-            }
-            for line in bom.bom_line_ids:
-                line_quantity = (bom_quantity) * line.product_qty
-                if line._skip_bom_line(product):
-                    continue
-                if line.child_bom_id:
-                    factor = line.product_uom_id._compute_quantity(line_quantity, line.child_bom_id.product_uom_id) * line.child_bom_id.product_qty
-                    price = self._get_price(line.child_bom_id, factor) / line_quantity
-                else:
-                    price = line.product_id.uom_id._compute_price(line.product_id.standard_price, line.product_uom_id)
-                prod_qty = line_quantity
-                total = prod_qty * price
-                components.append({
-                    'prod_id': line.product_id.id,
-                    'prod_name': line.product_id.display_name,
-                    'prod_qty': line_quantity,
-                    'prod_uom': line.product_uom_id.name,
-                    'prod_cost': price,
-                    'parent_id': bom_id,
-                    'line_id': line.id,
-                    'total': total,
-                    'child_bom': line.child_bom_id.id,
-                    'level': level or 0
-                })
-                lines['total'] += total
-            lines['components'] = components
         return {
             'lines': lines,
             'variants': bom_product_variants,
@@ -133,19 +69,85 @@ class MrpBomReport(models.TransientModel):
             'is_uom_applied': self.env.user.user_has_groups('uom.group_uom')
         }
 
+    def get_bom_line(self, bom_id=False, product_id=False, line_qty=False, line_id=False, level=False):
+        bom = self.env['mrp.bom'].browse(bom_id)
+        current_line = self.env['mrp.bom.line'].browse(int(line_id))
+        bom_quantity = current_line.product_uom_id._compute_quantity(line_qty, bom.product_uom_id)
+
+        # Display bom components for current selected product variant
+        if product_id:
+            product = self.env['product.product'].browse(int(product_id))
+        else:
+            product = bom.product_id or bom.product_tmpl_id.product_variant_id
+
+        components = []
+        operations = self._get_operation_line(bom.routing_id, (bom_quantity / bom.product_qty), 0)
+        lines = {
+            'bom': bom,
+            'bom_qty': bom_quantity,
+            'bom_prod_name': product.display_name,
+            'currency': self.env.user.company_id.currency_id,
+            'product': product,
+            'total': sum([op['total'] for op in operations]),
+            'level': level or 0,
+            'operations': operations,
+            'operation_ids': [op['operation'].id for op in operations],
+            'operations_cost': sum([op['total'] for op in operations]),
+            'operations_time': sum([op['duration_expected'] for op in operations])
+        }
+
+        for line in bom.bom_line_ids:
+            line_quantity = (bom_quantity) * line.product_qty
+            if line._skip_bom_line(product):
+                continue
+            if line.child_bom_id:
+                factor = line.product_uom_id._compute_quantity(line_quantity, line.child_bom_id.product_uom_id) * line.child_bom_id.product_qty
+                price = self._get_price(line.child_bom_id, factor) / line_quantity
+            else:
+                price = line.product_id.uom_id._compute_price(line.product_id.standard_price, line.product_uom_id)
+            prod_qty = line_quantity
+            total = prod_qty * price
+            components.append({
+                'prod_id': line.product_id.id,
+                'prod_name': line.product_id.display_name,
+                'prod_qty': line_quantity,
+                'prod_uom': line.product_uom_id.name,
+                'prod_cost': price,
+                'parent_id': bom_id,
+                'line_id': line.id,
+                'total': total,
+                'child_bom': line.child_bom_id.id,
+            })
+            lines['total'] += total
+        lines['components'] = components
+        return lines
+
     @api.model
-    def get_html(self, bom_id=False, searchQty=1, searchVariant=False, line_qty=False, line_id=False, level=False):
-        res = self.get_lines(bom_id=bom_id, searchQty=searchQty, searchVariant=searchVariant, line_qty=line_qty, line_id=line_id, level=level)
-        template = 'mrp.report_mrp_bom'
-        if level:
-            template = 'mrp.report_mrp_bom_line'
-        res['lines'] = self.env.ref(template).render({'data': res['lines']})
+    def get_operations(self, bom_id=False, qty=0, level=0):
+        bom = self.env['mrp.bom'].browse(bom_id)
+        lines = self._get_operation_line(bom.routing_id, qty, level)
+        values = {
+            'bom_id': bom_id,
+            'currency': self.env.user.company_id.currency_id,
+            'operations': lines,
+        }
+        return self.env.ref('mrp.report_mrp_operation_line').render({'data': values})
+
+    @api.model
+    def get_bom(self, bom_id=False, product_id=False, line_qty=False, line_id=False, level=False):
+        lines = self.get_bom_line(bom_id=bom_id, product_id=product_id, line_qty=line_qty, line_id=line_id, level=level)
+        return self.env.ref('mrp.report_mrp_bom_line').render({'data': lines})
+
+    @api.model
+    def get_html(self, bom_id=False, searchQty=1, searchVariant=False):
+        res = self.get_report_data(bom_id=bom_id, searchQty=searchQty, searchVariant=searchVariant)
+        res['lines'] = self.env.ref('mrp.report_mrp_bom').render({'data': res['lines']})
         return res
 
     @api.model
     def get_pdf(self, bom_id, child_bom_ids=[], searchQty=0, searchVariant=False):
         bom_id = self.env.context.get('active_id')
-        data = self.with_context(print_mode=True).get_lines(bom_id, report_type='pdf')
+        data = self.with_context(print_mode=True).get_report_data(bom_id, report_type='pdf')
         if not config['test_enable']:
             self = self.with_context(commit_assetsbundle=True)
         report_values = {
--- addons/mrp/static/src/js/mrp_bom_report.js
+++ addons/mrp/static/src/js/mrp_bom_report.js
@@ -15,41 +15,23 @@ var MrpBomReport = stock_report_generic.extend({
         'click .o_mrp_bom_foldable': '_onClickFold',
         'click .o_mrp_bom_action': '_onClickAction',
     },
-    get_html: function(event) {
+    get_html: function() {
         var self = this;
         var defs = [];
-        var $parent;
-        var activeID = this.given_context.active_id;
-        if(event){
-            $parent = $(event.currentTarget).closest('tr');
-            activeID = $parent.data('id');
-        }
         var args = [
-            activeID,
+            this.given_context.active_id,
             this.given_context.searchQty || 1,
             this.given_context.searchVariant || false,
         ];
-        if(event){
-            var lineID = $parent.data('line');
-            var qty = $parent.data('qty');
-            var level = $parent.data('level') || 0;
-            args.push(parseFloat(qty), lineID, level + 1);
-        }
         return this._rpc({
                 model: 'mrp.bom.report',
                 method: 'get_html',
                 args: args
             })
             .then(function (result) {
-                if(! event){
-                    self.data = result;
-                    defs.push(self.update_cp());
-                    return $.when.apply($, defs);
-                }
-                else{
-                    $parent.after(result.lines);
-                    $(event.currentTarget).toggleClass('o_mrp_bom_foldable o_mrp_bom_unfoldable fa-caret-right fa-caret-down');
-                }
+                self.data = result;
+                defs.push(self.update_cp());
+                return $.when.apply($, defs);
             });
     },
     set_html: function() {
@@ -60,11 +42,47 @@ var MrpBomReport = stock_report_generic.extend({
             self.update_cp();
         });
     },
-    _reload: function () {
-        var self = this;
-        return this.get_html().then(function () {
-            self.$el.html(self.data.lines);
-        });
+    get_bom: function(event) {
+      var $parent = $(event.currentTarget).closest('tr');
+      var activeID = $parent.data('id');
+      var productID = $parent.data('product_id');
+      var lineID = $parent.data('line');
+      var qty = $parent.data('qty');
+      var level = $parent.data('level') || 0;
+      return this._rpc({
+              model: 'mrp.bom.report',
+              method: 'get_bom',
+              args: [
+                  activeID,
+                  productID,
+                  parseFloat(qty),
+                  lineID,
+                  level + 1
+              ]
+          })
+          .then(function (result) {
+              $parent.after(result);
+              $(event.currentTarget).toggleClass('o_mrp_bom_foldable o_mrp_bom_unfoldable fa-caret-right fa-caret-down');
+          });
+    },
+    get_operations: function(event) {
+      var $parent = $(event.currentTarget).closest('tr');
+      var activeID = $parent.data('bom-id');
+      var qty = $parent.data('qty');
+      var level = $parent.data('level') || 0;
+      return this._rpc({
+              model: 'mrp.bom.report',
+              method: 'get_operations',
+              args: [
+                  activeID,
+                  parseFloat(qty),
+                  level + 1
+              ]
+          })
+          .then(function (result) {
+              $parent.after(result);
+              $(event.currentTarget).toggleClass('o_mrp_bom_foldable o_mrp_bom_unfoldable fa-caret-right fa-caret-down');
+          });
     },
     update_cp: function () {
         var status = {
@@ -111,20 +129,9 @@ var MrpBomReport = stock_report_generic.extend({
         this.given_context.searchVariant = $(ev.currentTarget).val();
         this._reload();
     },
-    _removeLines: function ($el) {
-        var self = this;
-        var activeID = $el.data('id');
-        _.each(this.$('tr[parent_id='+ activeID +']'), function (parent) {
-            var $parent = self.$(parent);
-            var $el = self.$('tr[parent_id='+ $parent.data('id') +']');
-            if ($el.length) {
-                self._removeLines($parent);
-            }
-            $parent.remove();
-        });
-    },
     _onClickUnfold: function (ev) {
-        this.get_html(ev);
+        var redirect_function = $(ev.currentTarget).data('function');
+        this[redirect_function](ev);
     },
     _onClickFold: function (ev) {
         this._removeLines($(ev.currentTarget).closest('tr'));
@@ -137,6 +144,25 @@ var MrpBomReport = stock_report_generic.extend({
             res_id: $(ev.currentTarget).data('res-id'),
             views: [[false, 'form']],
             target: 'current'
+        });
+    },
+    _reload: function () {
+        var self = this;
+        return this.get_html().then(function () {
+            self.$el.html(self.data.lines);
+        });
+    },
+    _removeLines: function ($el) {
+        var self = this;
+        var activeID = $el.data('id');
+        debugger;
+        _.each(this.$('tr[parent_id='+ activeID +']'), function (parent) {
+            var $parent = self.$(parent);
+            var $el = self.$('tr[parent_id='+ $parent.data('id') +']');
+            if ($el.length) {
+                self._removeLines($parent);
+            }
+            $parent.remove();
         });
     },
 });
