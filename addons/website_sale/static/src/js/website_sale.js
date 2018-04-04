23f77ba96de81d8629dc1c33601daa53694e93b7
Dharmraj Jhala <dja@odoo.com>
2018-04-04 12:09:23 +0530

    [FIX] website_sale: Prevent updation of cart qty in edit mode
    Because of the mechanism to bind change / click event in current implementation,
    when we are in edit mode of a product page, respected events are triggered on
    change of cart qty input (responsible class : js_quantity) or on click of
    + / - buttons (responsible class : js_add_cart_json). And this is responsible
    of triggering further events (calling update_product_image method, which
    causes this issue).
    
    After this commit, click / change event while updating cart qty will not be
    effective in edit mode, preventing further unwanted events being triggered.
    
    Related Task : 1825889
    PR : #24211

--- addons/website_sale/static/src/js/website_sale.js
+++ addons/website_sale/static/src/js/website_sale.js
@@ -107,7 +107,7 @@ $('.oe_website_sale').each(function () {
         var product_ids = [];
         var product_dom = $(".js_product .js_add_cart_variants[data-attribute_value_ids]");
         var qty = $(event.target).closest('form').find('input[name="add_qty"]').val();
-        if (!product_dom.length) {
+        if (!product_dom.length || $('body').hasClass('editor_enable')) {
             return;
         }
         _.each(product_dom, function(prod) {
@@ -149,7 +149,7 @@ $('.oe_website_sale').each(function () {
 
     $(oe_website_sale).on("change", ".oe_cart input.js_quantity[data-product-id]", function () {
       var $input = $(this);
-        if ($input.data('update_change')) {
+        if ($input.data('update_change') || $('body').hasClass('editor_enable')) {
             return;
         }
       var value = parseInt($input.val() || 0, 10);
@@ -219,8 +219,11 @@ $('.oe_website_sale').each(function () {
     });
 
 
-    // hack to add and rome from cart with json
+    // hack to add / remove from cart with json
     $(oe_website_sale).on('click', 'a.js_add_cart_json', function (ev) {
+        if ($('body').hasClass('editor_enable')) {
+            return;
+        }
         ev.preventDefault();
         var $link = $(ev.currentTarget);
         var $input = $link.parent().find("input");
