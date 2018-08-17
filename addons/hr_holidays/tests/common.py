3d76731414be1ff203a548534d82b7131889544d
Thibault Delavallée <tde@odoo.com>
2018-08-17 15:19:10 +0200

    [LNT] hr_holidays: small linting and remove unused views
    Purpose is to clean a bit the code to prepare some incoming modifications.
    Containing
    
     * small linting of leave fields;
     * remove some unused and unnecessary views and actions;
     * ensure default values for request_unit_all;
     * small linting of tests;
    
    This commit is linked to task ID 1876795 and PR #26423.

--- addons/hr_holidays/tests/common.py
+++ addons/hr_holidays/tests/common.py
@@ -9,35 +9,38 @@ class TestHrHolidaysBase(common.TransactionCase):
     def setUp(self):
         super(TestHrHolidaysBase, self).setUp()
 
-        Users = self.env['res.users'].with_context(no_reset_password=True)
-
-        # Find Employee group
-        group_employee_id = self.ref('base.group_user')
+        self._quick_create_ctx = {
+            'mail_create_nolog': True,
+            'mail_create_nosubscribe': True,
+            'mail_notrack': True,
+        }
+        self._quick_create_user_ctx = dict(self._quick_create_ctx, no_reset_password=True)
 
         # Test users to use through the various tests
+        Users = self.env['res.users'].with_context(self._quick_create_user_ctx)
         self.user_hruser_id = Users.create({
             'name': 'Armande HrUser',
             'login': 'Armande',
             'email': 'armande.hruser@example.com',
-            'groups_id': [(6, 0, [group_employee_id, self.ref('hr_holidays.group_hr_holidays_user')])]
+            'groups_id': [(6, 0, [self.ref('base.group_user'), self.ref('hr_holidays.group_hr_holidays_user')])]
         }).id
         self.user_hrmanager_id = Users.create({
             'name': 'Bastien HrManager',
             'login': 'bastien',
             'email': 'bastien.hrmanager@example.com',
-            'groups_id': [(6, 0, [group_employee_id, self.ref('hr_holidays.group_hr_holidays_manager')])]
+            'groups_id': [(6, 0, [self.ref('base.group_user'), self.ref('hr_holidays.group_hr_holidays_manager')])]
         }).id
         self.user_employee_id = Users.create({
             'name': 'David Employee',
             'login': 'david',
             'email': 'david.employee@example.com',
-            'groups_id': [(6, 0, [group_employee_id])]
+            'groups_id': [(6, 0, [self.ref('base.group_user')])]
         }).id
         self.user_hrmanager_2_id = Users.create({
             'name': 'Florence HrManager',
             'login': 'florence',
             'email': 'florence.hrmanager@example.com',
-            'groups_id': [(6, 0, [group_employee_id, self.ref('hr_holidays.group_hr_holidays_manager')])]
+            'groups_id': [(6, 0, [self.ref('base.group_user'), self.ref('hr_holidays.group_hr_holidays_manager')])]
         }).id
 
         # Hr Data
