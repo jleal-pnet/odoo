482dfce43dea33f2beafc9358f317647fd767002
Rémi Rahir <rar@odoo.com>
2018-08-22 14:15:41 +0200

    [IMP] snailmail_account: deactivate 'Send by post' option with the demo data

--- addons/snailmail_account/__manifest__.py
+++ addons/snailmail_account/__manifest__.py
@@ -8,6 +8,7 @@ Allows users to send invoices by post
     'category': 'Tools',
     'version': '0.1',
     'depends': ['account', 'snailmail'],
+    'demo': ['demo/snailmail_account_demo.xml'],
     'data': [
         'views/res_config_settings_views.xml',
         'wizard/account_invoice_send_views.xml',
