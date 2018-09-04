f40fb93203812528e7c297184c1b9930f4c3b218
Alexandre Kühn <aku@odoo.com>
2018-09-04 17:41:52 +0200

    [IMP] web: menu navbar in community
    Task-ID 1878649

---
+++ addons/web/static/src/js/chrome/apps_menu.js
@@ -0,0 +1,99 @@
+odoo.define('web.AppsMenu', function (require) {
+"use strict";
+
+var Widget = require('web.Widget');
+
+var AppsMenu = Widget.extend({
+    template: 'AppsMenu',
+    events: {
+        'click .o_apps_menu_item': '_onAppsMenuItemClicked',
+    },
+    /**
+     * @override
+     * @param {web.WebClient} parent
+     * @param {Object[]} menuData
+     * @param {Object[]} menuData.children
+     */
+    init: function (parent, menuData) {
+        this._super.apply(this, arguments);
+        this._activeApp = undefined;
+        this._apps = _.map(menuData.children, function (appMenuData) {
+            return {
+                actionID: parseInt(appMenuData.action.split(',')[1]),
+                menuID: appMenuData.id,
+                name: appMenuData.name,
+            };
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @returns {Object}
+     */
+    getActiveApp: function () {
+        return this._activeApp || {};
+    },
+    /**
+     * @returns {Object[]}
+     */
+    getApps: function () {
+        return this._apps;
+    },
+    /**
+     * Open the first app in the list of apps
+     */
+    openFirstApp: function () {
+        var firstApp = this._apps[0];
+        this._clickApp(firstApp);
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {Object} app
+     */
+    _clickApp: function (app) {
+        this._setActiveApp(app);
+        this.trigger_up('app_clicked', {
+            action_id: app.actionID,
+            menu_id: app.menuID,
+        });
+    },
+    /**
+     * @private
+     * @param {Object} app
+     */
+    _setActiveApp: function (app) {
+        this._activeApp = app;
+        this.renderElement();
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Called when clicking on an item in the apps menu.
+     *
+     * @private
+     * @param {MouseEvent} ev
+     */
+    _onAppsMenuItemClicked: function (ev) {
+        var $target = $(ev.currentTarget);
+        var actionID = $target.data('action-id');
+        var menuID = $target.data('menu-id');
+        var app = _.findWhere(this._apps, { actionID: actionID, menuID: menuID });
+        this._clickApp(app);
+    },
+
+});
+
+return AppsMenu;
+
+});
